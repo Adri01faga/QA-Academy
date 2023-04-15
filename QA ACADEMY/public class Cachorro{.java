ee73e646a259209343e972e7d291765d1523f6e0
Adri01faga <128923631+Adri01faga@users.noreply.github.com>
2023-04-15 15:00:51 -0300

    Add files via upload

---
+++ QA ACADEMY/public class Cachorro{.java
@@ -0,0 +1,10 @@
+public class Cachorro{ 
+    public String nome;
+    public float peso;
+    public float altura; // 6-7  casas decimais
+    public String cor;
+
+}//fim da classe Cachorro
+
+    
+}
